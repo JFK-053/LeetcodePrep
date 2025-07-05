ff81df51c83fc77ec4f4756a3fe05ea0b591502b
JFK-053 <agnethakamath@gmail.com>
2025-07-06 00:50:39 +0100

    Solution for Problem 217 - Contains Duplicate

---
+++ 217.ContainsDuplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        setNums = set(nums)
+        return len(setNums) != len(nums)
